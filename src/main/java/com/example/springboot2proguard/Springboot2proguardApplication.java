4aa0860b4e9de847a03f01b1d789f7811ec5000e
调兵 <gjy270020@alibaba-inc.com>
2021-06-23 17:04:24 +0800

    fix: add mybatis plus + XML support

--- src/main/java/com/example/springboot2proguard/Springboot2proguardApplication.java
+++ src/main/java/com/example/springboot2proguard/Springboot2proguardApplication.java
@@ -1,13 +1,27 @@
 package com.example.springboot2proguard;
 
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanNameGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Springboot2proguardApplication {
 
+    // 防止bean名称重复, 采用这个方法绕过
+    public static class CustomGenerator implements BeanNameGenerator {
+        @Override
+        public String generateBeanName(BeanDefinition definition, BeanDefinitionRegistry registry) {
+            System.out.println(definition.getBeanClassName());
+            return definition.getBeanClassName();
+        }
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(Springboot2proguardApplication.class, args);
+        SpringApplication springApplication =new SpringApplication(Springboot2proguardApplication.class);
+        springApplication.setBeanNameGenerator(new CustomGenerator());
+        springApplication.run(args);
     }
 
 }
